f739728db2e2888e4656d3d54422d11e62e9682d
elarbiaboussoror <elarbiaboussoror@visiativ.com>
2018-01-18 14:48:34 +0000

    Blank Java project for TDD katas

---
+++ src/main/java/ma/sccasa/fizzbuzzkata/FizzBuzzGenerator.java
@@ -0,0 +1,16 @@
+package ma.sccasa.fizzbuzzkata;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FizzBuzzGenerator {
+
+
+    public static List<Object> generateTill(int to) {
+
+    return null;
+
+    }
+
+
+}
---
+++ src/test/java/FizzBuzzTest.java
@@ -0,0 +1,19 @@
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.*;
+/**
+ * Test that FizzBuzzGenerator generates correct sequence for 100
+ * Game rule: Players take turns to count incrementally,
+ * replacing <b>any number divisible by three with the word "fizz"</b>,
+ * and <b>any number divisible by five with the word "buzz"</b>.
+ * <p>
+ * From: https://en.wikipedia.org/wiki/Fizz_buzz
+ */
+public class FizzBuzzTest {
+
+    @Test
+    public void testThatMultiplesOf3isReplacedByFizz() {
+        assertThat(false).isTrue();
+    }
+
+}
